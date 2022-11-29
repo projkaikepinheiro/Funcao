2839186dac48286db3d0e392efe3e63045b8d045
Kaike Pinheiro Simplicio <100208435+projkaikepinheiro@users.noreply.github.com>
2022-11-29 08:56:30 -0300

    Add files via upload

--- funcao.js
+++ funcao.js
@@ -1,3 +1,37 @@
 function soma(a,b){
     return a+b
 }
+
+function subtr(a,b){
+    return a-b
+}
+
+function mult(a,b){
+    return a*b
+}
+
+function div(a,b){
+    return a/b
+}
+
+function decisao(resp, pri, seg){
+    if(resp == 1){
+        resultado = soma(pri,seg)
+        document.write("<p>A soma é " + resultado,"</p>")
+    }
+
+    if(resp == 2){
+        resultado = subtr(pri,seg)
+        document.write("<p>A subtração é " + resultado,"</p>")
+    }
+
+    if(resp == 3){
+        resultado = mult(pri,seg)
+        document.write("<p>A multiplicação é " + resultado,"</p>")
+    }
+
+    if(resp == 4){
+        resultado = div(pri,seg)
+        document.write("<p>A divisão é " + resultado,"</p>")   
+    }
+}
